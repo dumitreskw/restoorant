2335552533df873bfcebae26096defa209361a89
dumitreskw <ovidumitresq@gmail.com>
2024-01-13 23:28:21 +0200

    add request for main page

--- app.js
+++ app.js
@@ -6,15 +6,12 @@ import cors from "cors";
 
 export const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:4200",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api/v1", user);
 app.use("/api/v1/cart", cart)
+app.get("/", (req, res) => {
+  res.send("Server is working");
+});
