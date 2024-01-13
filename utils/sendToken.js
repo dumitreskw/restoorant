025ec03ff088a07806548c637e8cb9d34ee5dabd
dumitreskw <ovidumitresq@gmail.com>
2024-01-14 00:20:07 +0200

    update cookie to allow cross origin

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -12,6 +12,7 @@ export const sendToken = (res, user, statusCode, message) => {
   const options = {
     httpOnly: false,
     expires: new Date(Date.now() + process.env.JWT_EXPIRE * 24 * 60 * 60 * 1000),
+    sameSite: 'None'
   };
 
   res
