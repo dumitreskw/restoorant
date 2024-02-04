ab9fe8981aba6faea72747a74af7cc6882678090
dumitreskw <ovidumitresq@gmail.com>
2024-02-04 13:06:46 +0200

    another attempt to fix cookies

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -14,7 +14,7 @@ export const sendToken = (res, user, statusCode, message) => {
     expires: new Date(Date.now() + process.env.JWT_EXPIRE * 24 * 60 * 60 * 1000),
     sameSite: 'None',
     secure: true,
-    domain: 'onrender.com',
+    domain: 'https://licenta.ovidiudumitrescu.com',
     path: '/'
   };
 
